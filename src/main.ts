c26001ff7311f3503f3864d24444214dd8f54456
Earl Narvacan <earl@appevolve.com>
2026-03-30 16:50:54 +0800

    immediately load env

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+// Load env immediately
+import dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import express from 'express';
